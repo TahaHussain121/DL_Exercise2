a60e16d6978ee689c278f6f9965483f795cdb58d
inahkazriMM <132692579+inahkazriMM@users.noreply.github.com>
2023-06-06 12:40:41 +0200

    Update Optimizers.py
    passes all "TestOptimizers2" now

--- exercise2_material/src_to_implement/Optimization/Optimizers.py
+++ exercise2_material/src_to_implement/Optimization/Optimizers.py
@@ -1,4 +1,4 @@
-
+import numpy as np
 
 class Sgd:
 
@@ -14,16 +14,35 @@ class Sgd:
 class SgdWithMomentum:
 
     def __init__(self, learning_rate, momentum_rate):
-        pass
+        self.v_k = 0
+        self.momentum_rate = momentum_rate
+        self.learning_rate = learning_rate
 
     def calculate_update(self, weight_tensor, gradient_tensor):
-        pass
+        self.v_k = self.momentum_rate * self.v_k - (self.learning_rate * gradient_tensor)
+        return weight_tensor + self.v_k
 
 
 class Adam:
 
     def __init__(self, learning_rate, mu, rho):
-        pass
+        self.learning_rate = learning_rate
+        self.mu = mu
+        self.rho = rho
+        self.g_k = 0    # k refers to the kth step/iteration of the gradient update
+        self.v_k = 0
+        self.r_k = 0
+        self.counter_k = 0
 
     def calculate_update(self, weight_tensor, gradient_tensor):
-        pass
+        self.counter_k += 1
+        self.g_k = gradient_tensor
+        self.v_k = self.mu * self.v_k + (1 - self.mu) * self.g_k
+        self.r_k = self.rho * self.r_k + np.multiply((1 - self.rho) * self.g_k, self.g_k)
+
+        # bias correction
+        v_hat = self.v_k / (1 - self.mu**self.counter_k)
+        r_hat = self.r_k / (1 - self.rho**self.counter_k)
+
+        return weight_tensor - self.learning_rate * (v_hat / (np.sqrt(r_hat) + np.finfo(np.float64).eps))
+    
