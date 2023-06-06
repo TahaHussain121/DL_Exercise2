9ff4ab6f0aed9e398d46df1186dbe62daf08c0ca
inahkazriMM <132692579+inahkazriMM@users.noreply.github.com>
2023-06-06 12:18:02 +0200

    Update Flatten.py
    should pass all respective tests now

--- exercise2_material/src_to_implement/Layers/Flatten.py
+++ exercise2_material/src_to_implement/Layers/Flatten.py
@@ -4,10 +4,20 @@
 class Flatten:
 
     def __init__(self):
-        pass
+        self.trainable = False  # maybe Flaaten class should inherit from the base layer and then change it like in FullycConnected.py for example
+        self.shape = None
 
     def forward(self, input_tensor):
-        pass
+        # store shape of input so you can access it in the backward method
+        self.shape = input_tensor.shape
+
+        number_of_weights = 1
+        # only flatten from dim 2 onwards, because first dim seems to contain the different weight "blocks"
+        for i in list(input_tensor.shape)[1:]:
+            number_of_weights *= i
+
+        return np.reshape(input_tensor, (input_tensor.shape[0], number_of_weights))
 
     def backward(self, error_tensor):
-        pass
+
+        return np.reshape(error_tensor, self.shape)
